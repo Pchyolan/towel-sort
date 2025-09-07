273bbde22432708f209614e196c2609bd50e4a8b
unknown <mozhno_prost_kostya@mail.ru>
2025-09-07 23:34:58 +0300

    feat: create towelsort function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    const miniArr = matrix[i];
+    if (i % 2 !== 0) {
+      miniArr.reverse();
+    }
+    result.push(...miniArr);
+  }
+
+  return result;
 };
